e5e957dbd2beac94ae15e9132444c470c0baa5af
Nandana V <nandana.v@experionglobal.local>
2022-11-04 12:51:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+var card = document.getElementById("card");
+
+function openRegister(){
+    card.style.transform = "rotateY(-180deg)";
+}
+
+function openLogin(){
+    card.style.transform = "rotateY(0deg)";
+}
+
+function openPayment(){
+    card.style.transform = "rotateY(-180deg)";
+}
+
+function openCard(){
+    card.style.transform = "rotateY(-180deg)";
+    
+}
+
+function openUpi(){
+    card.style.transform = "rotateY(-180deg)";
+}
+
+function openCash(){
+    card.style.transform = "rotateY(-180deg)";
+}
